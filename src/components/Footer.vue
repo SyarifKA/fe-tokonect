abc8e80fefb338ef863ebd80c2aa044d4a3004d0
Syarif09 <syarif.khalid@gmail.com>
2025-05-21 14:56:52 +0700

    register page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,15 +3,15 @@
 </script>
 
 <template>
-    <div class="bg-white">
-        <div>
-            <div class="flex flex-col gap-4">
-                <div>TOKONECT - 1st NYC TECH ONLINE MARKET</div>
+    <div class="bg-white flex flex-col py-8 px-4">
+        <div class="flex gap-8">
+            <div class="flex flex-col gap-4 items-start pb-16">
+                <div class="font-medium">TOKONECT - 1st NYC TECH ONLINE MARKET</div>
                 <div class="flex flex-col">
                     <span>HOTLINE 24/7</span>
                     <span class="text-3xl text-[#1ABA1A] font-bold">(025) 3686 25 16</span>
                 </div>
-                <div class="flex flex-col">
+                <div class="flex flex-col text-sm">
                     <span>257 Thatcher Road St, Brooklyn, Manhattan, NY 10092</span>
                     <span>contact@Swootechmart.com</span>
                 </div>
@@ -29,12 +29,77 @@
                         <v-icon name="bi-youtube"/>
                     </button>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
             </div>
-            <div></div>
+            <div class="flex flex-col gap-6 text-black">
+                <div class="font-medium">TOP CATEGORIES</div>
+                <div class="flex flex-col text-[#666666] gap-2 items-start text-sm">
+                    <button>Laptops</button>
+                    <button>PC & Computers</button>
+                    <button>Cell Phones</button>
+                    <button>Tablets</button>
+                    <button>Gaming & VR</button>
+                    <button>Networks</button>
+                    <button>Cameras</button>
+                </div>
+            </div>
+            <div class="flex flex-col gap-4 text-black">
+                <div class="font-medium">COMPANY</div>
+                <div class="flex flex-col text-[#666666] gap-2 items-start text-sm">
+                    <button>About Tokonect</button>
+                    <button>Contact</button>
+                    <button>Career</button>
+                    <button>Blog</button>
+                    <button>Sitemap</button>
+                    <button>Store Locations</button>
+                </div>
+            </div>
+            <div class="flex flex-col gap-4 text-black">
+                <div class="font-medium">HELP CENTER</div>
+                <div class="flex flex-col text-[#666666] gap-2 items-start text-sm">
+                    <button>Policy</button>
+                    <button>Terms & Conditions</button>
+                    <button>Trach Order</button>
+                    <button>FAQs</button>
+                    <button>My Account</button>
+                    <button>Product Support</button>
+                </div>
+            </div>
+            <div class="flex flex-col gap-4 text-black">
+                <div class="font-medium">PARTNER</div>
+                <div class="flex flex-col text-[#666666] gap-2 items-start text-sm">
+                    <button>Become Seller</button>
+                    <button>Affiliate</button>
+                    <button>Advertise</button>
+                    <button>Partnership</button>
+                </div>
+            </div>
         </div>
-        <div></div>
+        <div class="flex flex-col gap-4">
+            <div>
+                <span class="font-medium">SUBCRIBE & GET <span class="text-red-600">10% OFF</span> FOR YOUR FIRST ORDER</span>
+            </div>
+            <div class="flex w-1/3 border-b border-[#757575] justify-between px-2">
+                <input type="text" placeholder="Enter your email address" class="w-full text-[#757575]">
+                <button class="text-[#1ABA1A] font-medium">SUBSCRIBE</button>
+            </div>
+            <div>
+                <span class="text-[#757575]">By subscribing, you’re accepted the our <button class="text-black underline">Policy</button></span>
+            </div>
+        </div>
+        <div class="flex pt-8 border-[#757575] border-t mt-16 justify-between">
+            <div>
+                <span class="text-[#757575]">© 2024 <span class="text-black">Shawonetc3</span> . All Rights Reserved</span>
+            </div>
+            <div class="flex gap-4">
+                <img src="../assets/img/klarna.png" alt="">
+                <img src="../assets/img/master.png" alt="">
+                <img src="../assets/img/visa.png" alt="">
+                <img src="../assets/img/stripe.png" alt="">
+                <img src="../assets/img/paypal.png" alt="">
+            </div>
+            <div>
+                <button class="text-blue-500">Mobile site</button>
+            </div>
+        </div>
     </div>
 </template>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,8 +6,41 @@ import Footer from '../components/Footer.vue'
 <template>
     <div>
         <Navbar />
-        <main>
-            
+        <main class="flex justify-around gap-8 p-10 mx-8 my-4 rounded-lg bg-white">
+            <div class="flex w-1/2 h-[300px] justify-center">
+                <img src="../assets/img/register.png" alt="register" class="object-cover">
+            </div>
+            <div class="w-1/2 gap-4 flex px-8 flex-col">
+                <div class="flex flex-col w-full">
+                    <span class="text-[#1ABA1A] font-medium">REGISTER</span>
+                    <span class="text-sm text-[#999999]"> JOIN TO US</span>
+                </div>
+                <form action="" class="w-full flex flex-col gap-4">
+                    <div class="flex flex-col gap-2 text-sm">
+                        <label for="name">Your name</label>
+                        <input type="text" id="name" placeholder="Jhon Doe" class="text-[#999] border w-full py-2 rounded-lg px-4 border-[#CCC]">
+                    </div>
+                    <div class="flex flex-col gap-2 text-sm">
+                        <label for="email">Email Address</label>
+                        <input type="email" id="email" placeholder="Example@gmail.com" class="text-[#999] border w-full py-2 rounded-lg px-4 border-[#CCC]">
+                    </div>
+                    <div class="flex flex-col gap-2 text-sm">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" placeholder="****" class="text-[#999] border w-full py-2 rounded-lg px-4 border-[#CCC]">
+                    </div>
+                    <div class="flex flex-col gap-2 text-sm">
+                        <label for="cpassword">Confirm password</label>
+                        <input type="password" id="cpassword" placeholder="****" class="text-[#999] border w-full py-2 rounded-lg px-4 border-[#CCC]">
+                    </div>
+                    <div class="text-sm">
+                        <button type="submit" class="text-white bg-[#1ABA1A] px-4 py-2 rounded-lg">REGISTER</button>
+                    </div>
+                    <div class="flex">
+                        <span class="text-[#999]">already user ?</span>
+                        <button class="text-[#1ABA1A]"> Login</button>
+                    </div>
+                </form>
+            </div>
         </main>
         <Footer />
     </div>
